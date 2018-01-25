e4512c132d6de227dd7e0ad7556e0ea56b424b6d
Kevin Yan <k3vinyan@gmail.com>
2018-01-24 16:11:37 -0800

    import react and add script to index.html

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+class Layout extends React.Component {
+  render(){
+    return(
+      <h1>This is from React</h1>
+    )
+  }
+}
+
+ReactDOM.render(<Layout />, document.getElementById('app'))
