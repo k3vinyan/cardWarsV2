f00aba446e4e7202e90497ffd402861e32673f73
Kevin Yan <k3vinyan@gmail.com>
2018-01-24 19:05:11 -0800

    import babel-preset-stage-0 package

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import LoginForm from './LoginForm';
+import io from 'socket.io-client';
 
+const socketUrl = "https://localhost:3000";
 export default class Layout extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      socket:null
+    };
+  }
+
+  componentWillMount=()=>{
+
+  }
+
+  initSocket = ()=>{
+    const socket = io(socketUrl)
+    socket.on('connect', ()=>{
+      console.log("Connected")
+    })
+    this.setState({socket})
+  }
+
   render(){
+    const { title } = this.props;
     return(
-      <LoginForm />
+      <div>
+        <LoginForm />
+      </div>
     )
   }
 }
